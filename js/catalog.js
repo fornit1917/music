f62c1fdd4b3e3102b4906425b719761f0db482d8
Vit S <fornit1917@gmail.com>
2016-08-17 19:56:43 +0300

    fix navigation

--- js/catalog.js
+++ js/catalog.js
@@ -1,5 +1,6 @@
 var Catalog = {
-	data: null
+	data: null,
+	currentCategory: null
 };
 
 Catalog.init = function () {
@@ -10,24 +11,27 @@ Catalog.init = function () {
 		}
 		var $this = $(this);
 		var categoryId = $this.attr('data-category-id');
+
 		var playlists = Catalog.data[categoryId];
 		if (!playlists || playlists.length === 0) {
 			console.log('Playlist ' + categoryId + ' is empty');
 			return;
 		}
 
-		var html = [];
-		for (var i=0; i<playlists.length; i++) {
-			html.push(playlists[i].html);
-		}
+		if (categoryId !== Catalog.currentCategory) {
+			var html = [];
+			for (var i=0; i<playlists.length; i++) {
+				html.push(playlists[i].html);
+			}
 
-		$('#playlists').html(html.join(''));
+			$('#playlists').html(html.join(''));
+		}
 		$('#music').show();
 		$('#category-list').hide();
+		Catalog.currentCategory = categoryId;
 	});
 
 	$('#back-to-categories').on('click', function () {
-		$('#playlists').html('');
 		$('#music').hide();
 		$('#category-list').show();
 	});
