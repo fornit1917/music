b6c7128c99e086b89fdd21de3a46cadd155fca09
Vit S <fornit1917@gmail.com>
2016-08-17 22:34:52 +0300

    tag filter

--- js/catalog.js
+++ js/catalog.js
@@ -1,10 +1,25 @@
 var Catalog = {
 	data: null,
-	currentCategory: null
+	currentCategory: null,
+	filterTimeoutId: 0,
+
+	$filterField: null,
+	$categoriesScreen: null,
+	$musicScreen: null,
+	$playlists: null,
+
+	currentFilterQuery: '',
 };
 
 Catalog.init = function () {
+
+	Catalog.$categoriesScreen = $('#category-list');
+	Catalog.$musicScreen = $('#music');
+	Catalog.$playlists = $('#playlists');
+	Catalog.$filterField = $('#filter');
+
 	Catalog.load();
+
 	$('.category-item').on('click', function () {
 		if (Catalog.data === null) {
 			return;
@@ -19,22 +34,20 @@ Catalog.init = function () {
 		}
 
 		if (categoryId !== Catalog.currentCategory) {
-			var columns = [[],[]];
-			for (var i=0; i<playlists.length; i++) {
-				columns[i % 2].push(playlists[i].html);
-			}
-			var html = '<div class="playlists-column">' + columns[0].join('') + '</div>';
-			html += '<div class="playlists-column">' + columns[1].join('') + '</div>';
-			$('#playlists').html(html);
+			Catalog.showPlaylists(Catalog.data[categoryId]);
 		}
-		$('#music').show();
-		$('#category-list').hide();
+		Catalog.$musicScreen.show();
+		Catalog.$categoriesScreen.hide();
 		Catalog.currentCategory = categoryId;
 	});
 
+	Catalog.$filterField.on('input', function () {
+		Catalog.restartFilterTimeout();
+	});
+
 	$('#back-to-categories').on('click', function () {
-		$('#music').hide();
-		$('#category-list').show();
+		Catalog.$musicScreen.hide();
+		Catalog.$categoriesScreen.show();
 	});
 };
 
@@ -44,7 +57,70 @@ Catalog.load = function () {
 			if (typeof resp === 'string') {
 				resp = JSON.parse(resp);
 			}
+			for (var key in resp) {
+				for (var i = 0; i < resp[key].length; i++) {
+					resp[key][i].tags = resp[key][i].tags.map(function (item) { return item.toLowerCase() })
+				}
+			}
 			Catalog.data = resp;
 		});
 };
 
+Catalog.restartFilterTimeout = function () {
+	if (Catalog.filterTimeoutId !== 0) {
+		clearTimeout(Catalog.filterTimeoutId);
+	}
+	Catalog.filterTimeoutId = setTimeout(function () {
+		var query = Catalog.$filterField.val();
+		query = query.toLowerCase();
+		if (query === Catalog.currentFilterQuery) {
+			return;
+		}
+		Catalog.currentFilterQuery = query;
+		
+		var playlists = Catalog.filterPlaylists(query);
+		Catalog.showPlaylists(playlists);
+	}, 1000);
+};
+
+Catalog.filterPlaylists = function (query) {
+	var playlists = Catalog.data[Catalog.currentCategory];
+	if (query.trim() === "") {
+		return playlists;
+	}
+	return playlists.filter(function (item) {
+		for (var key in item.tags) {
+			if (Catalog.strStartsWith(item.tags[key], query)) {
+				return true;
+			}
+		}
+		return false;
+	});
+};
+
+Catalog.showPlaylists = function (playlists) {
+	var columns = [[],[]];
+	for (var i = 0; i < playlists.length; i++) {
+		columns[i % 2].push(playlists[i].html);
+	}
+	var html = '<div class="playlists-column">' + columns[0].join('') + '</div>';
+	html += '<div class="playlists-column">' + columns[1].join('') + '</div>';
+	Catalog.$playlists.html(html);
+};
+
+
+Catalog.strStartsWith = function (str, query) {
+	if (typeof String.prototype.startsWith === 'function') {
+		return str.startsWith(query);
+	} else {
+		if (query.length > str.length) {
+			return false;
+		}
+		for (var i = 0; i < query.length; i++) {
+			if (str.charAt(i) != query.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
